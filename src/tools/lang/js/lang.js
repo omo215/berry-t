124a63fd63e4f654da38c4e99f886005a395ce4b
omo215 <58347492+omo215@users.noreply.github.com>
2023-03-21 14:37:41 +0000

    0321

--- src/tools/lang/js/lang.js
+++ src/tools/lang/js/lang.js
@@ -1,6 +1,3 @@
-eruda.init()
-console.log('start')
-const jaButton = document.getElementById('JO_JP_button')
-jaButton.addEventListener('click', function (e) {
-    console.log(e)
-})
+function test(lang){
+    console.log(lang)
+}
