22d2132b7d8210f1080321c64f9d67d39c6e6aa2
omo215 <58347492+omo215@users.noreply.github.com>
2023-03-22 15:03:55 +0000

    03

--- src/tools/lang/js/lang.js
+++ src/tools/lang/js/lang.js
@@ -1,24 +1,72 @@
+const API_KEY = "ec69e56b-58f0-d83e-397a-7c54fa95c196:fx";
+const API_URL = "https://api-free.deepl.com/v2/translate";
 
+async function langChange(from_id,to_id, from, to) {
+  const entext = document.getElementById(from_id).value;
 
-const API_KEY = 'ec69e56b-58f0-d83e-397a-7c54fa95c196:fx';
-const API_URL = 'https://api-free.deepl.com/v2/translate';
+  let content = encodeURI(
+    "auth_key=" +
+      API_KEY +
+      "&text=" +
+      entext +
+      "&source_lang=" +
+      from +
+      "&target_lang=" +
+      to
+  );
+  let url = API_URL + "?" + content;
+  const res = await fetch(url);
+  const val = res.json();
+  document.getElementById(to_id).value = val["translations"][0]["text"];
+}
+//   fetch(url)
+//     .then(function (response) {
+//       if (response.ok) {
+//         return response.json();
+//       } else {
+//         throw new Error("Could not reach the API: " + response.statusText);
+//       }
+//     })
+//     .then(function (data) {
+//       document.getElementById("JO_EN_text").value =
+//         data["translations"][0]["text"];
+//     })
+//     .catch(function (error) {
+//       document.getElementById("JO_EN_text").value = error.message;
+//     });
+function fromJA(){
+    langChange('JO_JP_text','JO_EN_text','JA','EN')
+    langChange('JO_JP_text','JO_PT_text','JA','PT')
+    langChange('JO_JP_text','JO_ID_text','JA','ID')
+    
+}
 
-function langChange(id) {
-  const entext = document.getElementById(id).value;
-
-  let content = encodeURI('auth_key=' + API_KEY + '&text=' + entext + '&source_lang=EN&target_lang=JA');
-  let url = API_URL + '?' + content;
-
-  fetch(url)
-    .then(function(response) {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error("Could not reach the API: " + response.statusText);
-        }
-    }).then(function(data) {
-        document.getElementById("JO_EN_text").value = data["translations"][0]["text"];
-    }).catch(function(error) {
-        document.getElementById("JO_EN_text").value = error.message;
-    });
-};
+// BG - Bulgarian
+// CS - Czech
+// DA - Danish
+// DE - German
+// EL - Greek
+// EN - English
+// ES - Spanish
+// ET - Estonian
+// FI - Finnish
+// FR - French
+// HU - Hungarian
+// ID - Indonesian
+// IT - Italian
+// JA - Japanese
+// KO - Korean
+// LT - Lithuanian
+// LV - Latvian
+// NB - Norwegian (Bokmål)
+// NL - Dutch
+// PL - Polish
+// PT - Portuguese (all Portuguese varieties mixed)
+// RO - Romanian
+// RU - Russian
+// SK - Slovak
+// SL - Slovenian
+// SV - Swedish
+// TR - Turkish
+// UK - Ukrainian
+// ZH - Chinese
